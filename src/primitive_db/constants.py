2ecd78a8291f6ec6d237667829a948b0117d3c71
subterraneanbob <4823336+subterraneanbob@users.noreply.github.com>
2025-10-17 21:27:13 +0300

    Implement create table function

--- src/primitive_db/constants.py
+++ src/primitive_db/constants.py
@@ -1,1 +1,6 @@
 DB_META_FILE = "db_meta.json"
+
+ID_COLUMN_NAME = "ID"
+ID_COLUMN_DATA_TYPE = "int"
+
+SUPPORTED_DATA_TYPES = ("int", "str", "bool")
---
+++ src/primitive_db/core.py
@@ -0,0 +1,52 @@
+from collections.abc import Iterable
+
+from .constants import ID_COLUMN_DATA_TYPE, ID_COLUMN_NAME, SUPPORTED_DATA_TYPES
+
+
+def create_table(metadata: dict, table_name: str, columns: Iterable[str]) -> dict:
+    """
+    Добавляет новую таблицу с указанными столбцами в словарь метаданных, если
+    таблицы с таким именем не существует. Столбец "ID:int" будет добавлен
+    автоматически.
+
+    Выводит ошибку, если:
+    - таблица уже существует
+    - указан некорректный тип данных (поддерживаются: int | str | bool)
+
+    Args:
+        metadata (dict): Текущие метаданные
+        table_name (str): Название новой таблицы
+        columns (Iterable[str]): Список столбцов в виде "название:тип_данных"
+
+    Returns:
+        dict: Обновлённый словарь метаданных.
+    """
+
+    if table_name in metadata:
+        print(f'Ошибка: Таблица "{table_name}" уже существует.')
+        return metadata
+
+    table_metadata = {ID_COLUMN_NAME: ID_COLUMN_DATA_TYPE}
+
+    for column in columns:
+        name, _, data_type = map(str.strip, column.partition(":"))
+
+        if data_type not in SUPPORTED_DATA_TYPES:
+            print(f'Некорректное значение: "{column}". Попробуйте снова.')
+            return metadata
+
+        # Столбец ID всегда добавляется автоматически, даже если указан явно
+        if name == ID_COLUMN_NAME:
+            continue
+
+        table_metadata[name] = data_type
+
+    metadata[table_name] = table_metadata
+
+    created_columns = [":".join(item) for item in table_metadata.items()]
+    print(
+        f'Таблица "{table_name}" успешно создана '
+        f"со столбцами: {', '.join(created_columns)}"
+    )
+
+    return metadata
